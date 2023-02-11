b08aa5cfb8749b9c0c998c7b90fb9a4aede8bb2e
bkhzhou <79166987+bkhzhou@users.noreply.github.com>
2023-02-11 11:52:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ app.use(express.json());
 
 app.get('/',  (req, res) => {
     console.log('Successful')
+    res.send('Successful')
 })
 
 const itemsRouter = require('./routes/items');
 
-app.use('/items', itemsRouter)
+app.use('/items', itemsRouter);
 
 app.listen(3000, () => console.log('Server listening on port 3000'));
